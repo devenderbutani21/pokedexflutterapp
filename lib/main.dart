16995b9eb02489c7c3e8baa36c3c8afabf1a7129
devenderbutani21 <devenderbutani21@gmail.com>
2020-10-22 23:32:13 -0400

    API changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import './models/pokemon_model.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,24 +23,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+  Pokedex pokedex;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(url);
+    var decodedJson = jsonDecode(res.body);
+
+    pokedex = Pokedex.fromJson(decodedJson);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: CircleAvatar(
-            backgroundColor: Color(0xff156aa2),
-            radius: 1.0,
-          ),
-          title: Text(
-            'Pokedex',
-          ),
-          backgroundColor: Color(0xffdc0a2d),
+    return Scaffold(
+      appBar: AppBar(
+        leading: CircleAvatar(
+          backgroundColor: Color(0xff156aa2),
+          radius: 1.0,
         ),
-        body: Container(
-
+        title: Text(
+          'Pokedex',
         ),
+        backgroundColor: Color(0xffdc0a2d),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: pokedex.pokemon.map((poke) => Card()).toList(),
       ),
     );
   }
